eb89c428e3df4bbc020d49e5d835b4cfa299a0d3
Alejandro Quivera <35770411+Alequi@users.noreply.github.com>
2025-12-28 16:39:25 +0100

    feat: implement header and footer partials; refactor pages to include them for consistency

---
+++ public/partials/header.php
@@ -0,0 +1,25 @@
+
+  <div class="container-xxl">
+    <header
+      class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-light text-dark">
+
+      <div class="col-md-3 mb-2 mb-md-0">
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+          <img src="../public/assets/brand/LOGO-CES-2.png" alt="Logo" width="100" height="auto" />
+        </a>
+      </div>
+      <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+
+        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li><a href="../index.html" class="nav-link px-2 ">Inicio</a></li>
+          <li><a href="about.html" class="nav-link px-2">Sobre el proyecto</a></li>
+
+        </ul>
+      </ul>
+      <div class="col-md-3 text-end">
+        <a href="login.php"><button type="button" class="btn btn-outline-primary me-2">Login</button></a>
+
+        <a href="registro.php"><button type="button" class="btn btn-primary">Registro</button></a>
+      </div>
+    </header>
+  </div>
