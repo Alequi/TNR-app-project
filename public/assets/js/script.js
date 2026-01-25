60349fd8d8408fa93019c033dfeb93041cb822d8
Alejandro Quivera <35770411+Alequi@users.noreply.github.com>
2026-01-25 16:04:27 +0100

    feat: move modal reservation logic to a separate file for better organization

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -13,20 +13,3 @@ function pass() {
       }
 }
 
-// Modal reserva turno
-
-const reserveModal = document.getElementById('reserveModal');
-          reserveModal.addEventListener('show.bs.modal', function (event) {
-            const button = event.relatedTarget;
-            
-            const shiftId = button.getAttribute('data-shift-id');
-            const shiftFecha = button.getAttribute('data-shift-fecha');
-            const shiftClinica = button.getAttribute('data-shift-clinica');
-            const shiftTurno = button.getAttribute('data-shift-turno');
-            
-            document.getElementById('shift_id').value = shiftId;
-            document.getElementById('shift_fecha').textContent = shiftFecha;
-            document.getElementById('shift_clinica').textContent = shiftClinica;
-            document.getElementById('shift_turno').textContent = shiftTurno;
-            document.getElementById('numero_gatos').value = '';
-          });
