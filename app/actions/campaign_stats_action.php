43367759914ac0a1b2786eade4fc6ad3da594918
Alejandro Quivera <35770411+Alequi@users.noreply.github.com>
2026-01-31 19:47:05 +0100

    fix: call admin function to ensure proper authorization

--- app/actions/campaign_stats_action.php
+++ app/actions/campaign_stats_action.php
@@ -3,7 +3,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 require_once __DIR__ . '/../helpers/auth.php';
-admin();
+
 
 require_once __DIR__ . '/../../config/conexion.php';
 $con = conectar();
